9ff308d30567a6a09906dca57e3214116bae7aa7
devesh tripathi <deveshtrip10@gmail.com>
2026-04-23 20:32:11 +0530

    new day new commit

--- day_1.py
+++ day_1.py
@@ -1,1 +1,14 @@
-print("hello")
+# what is python?
+"""Python is a high-level, general-purpose, interpreted programming language known for its simple, English-like syntax and emphasis on code readability."""
+
+"""Created by Guido van Rossum and first released in 1991, it supports multiple programming paradigms including object-oriented, procedural, and functional styles."""
+
+print("Hello World!")
+# Here print is a in-built function
+
+var = 12
+# Here var is a variable which act as a container for the value.
+
+# The name given to the variable is called "identifier" and the value is called a "literal"
+
+# One important thing in python is indentation, it is used to define the scope and structure of the code, python code can work on even one indent but the standard indent is 4 (4 spaces)
