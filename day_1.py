6e005a63ccd764944336ebba4f1cd27115cce150
devesh tripathi <deveshtrip10@gmail.com>
2026-04-26 19:59:04 +0530

    new commit

--- day_1.py
+++ day_1.py
@@ -12,3 +12,22 @@ var = 12
 # The name given to the variable is called "identifier" and the value is called a "literal"
 
 # One important thing in python is indentation, it is used to define the scope and structure of the code, python code can work on even one indent but the standard indent is 4 (4 spaces)
+
+# Data Types
+"""Python has two important features:
+1. Dynamic Typing: When you want to specify the datatype at code runtime, that phenomenon is called dynamic typing
+Example: name = input() {now at runtime we can specify any datatype}
+
+2. Dynamic Binding: Variables can change the nature of there dataype at runtime
+Example: num = 4;
+         num = 'Hello' {if you check the type of num it will be string}"""
+
+# Integer: int(whole numbers which can be negative or positive)
+
+num = 4
+
+# Floatation = float(Decimal number which can be negative or positive)
+
+num = 4.0
+
+# String
