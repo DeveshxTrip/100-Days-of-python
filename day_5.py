4a87acb5d026f8df0d6633eb37003ef96c311daa
devesh tripathi <deveshtrip10@gmail.com>
2026-05-11 14:56:45 +0530

    yayy

--- day_5.py
+++ day_5.py
@@ -1,4 +1,13 @@
 # Strings
 
-my_str = "Hello World"
-print(my_str[0:5])
+# my_str = "Hello World"
+# print(my_str[0:5])
+
+str_1 = "abbebba"
+
+reverse = str_1[::-1]
+
+if reverse == str_1:
+    print("palindrome")
+else:
+    print("Not a palindrome")
