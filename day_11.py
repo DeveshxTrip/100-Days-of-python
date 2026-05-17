baf7b55062a2f3aa41b61ee2affe9ae517c9696a
devesh tripathi <deveshtrip10@gmail.com>
2026-05-17 20:56:29 +0530

    learning

--- day_11.py
+++ day_11.py
@@ -5,8 +5,13 @@ S2 = {10}
 print(type(S1))
 print(type(S2))
 n = 0
-while n <= 4:
-    name = input("Enter the names: ")
-    S2.add(name)
-    n += 1
-print(S2)
+# while n <= 4:
+#     name = input("Enter the names: ")
+#     S2.add(name)
+#     n += 1
+# print(S2)
+
+S3 = {"Hello", (1, 2, 4)}
+print(S3)
+
+print((1, 2, 4) in S3)
