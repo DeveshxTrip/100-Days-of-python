1dc18eb2fdac76153535cba69ab229d2a35cbdde
devesh tripathi <deveshtrip10@gmail.com>
2026-06-13 20:37:48 +0530

    ddef

--- day_16.py
+++ day_16.py
@@ -3,3 +3,16 @@ def hello():
 
 
 hello()
+
+
+def oddEven(num):
+    if num % 2 == 0:
+        print("Even")
+        return "even"
+    else:
+        print("Odd")
+        return "odd"
+
+
+result = oddEven(4)
+print(result)
