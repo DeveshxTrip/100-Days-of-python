12cc79d97dd10ac426ba24bd19777beaf9a8a7de
devesh tripathi <deveshtrip10@gmail.com>
2026-06-25 00:30:57 +0530

    d3ew

--- day_20.py
+++ day_20.py
@@ -1,11 +1,10 @@
-random_list = []
+random_list = [1, 2, 1, 2, 3, 1, 3, 2, 4, 1, 5, 4, 5, 6]
 my_list = []
 
 for i in random_list:
     if i not in my_list:
         my_list.append(i)
     else:
-        print("already in the list")
         continue
 
 
